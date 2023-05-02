be6fcc483ad5a5f6a389628129889cca56a1a67b
Fernando Garcia <fernando.garcia@mail.com>
2023-05-02 13:03:00 -0500

    add update 2

--- src/socket/_index.js
+++ src/socket/_index.js
@@ -1,7 +1,11 @@
 module.exports = (server) => {
   const { Server } = require("socket.io");
 
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "*"
+    }
+  });
 
   require("./addSocketMiddlewares")(io);
 
